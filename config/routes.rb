30784786cc6fb257187c41a7c92c6042283ef97d
Andrew Hao <ahao@blurb.com>
2014-05-31 10:14:35 -0700

    Updated routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Mailbox::Application.routes.draw do
-  root :to => "visitors#index"
+  root :to => "home#index"
   resources :users, :only => [:index, :show, :edit, :update ]
   get '/auth/:provider/callback' => 'sessions#create'
   get '/signin' => 'sessions#new', :as => :signin
