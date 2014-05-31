bd574d86ea9d721bc00d0fca4e688f03deabf0a2
Andrew Hao <ahao@blurb.com>
2014-05-31 09:18:22 -0700

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mailbox::Application.config.secret_key_base = 'de4b38d6a01ef84472e09819cb484b0801e8f564e02df0ecc7829ef50d1857278d757e4191b459df9e1e0d34d0defd7d186e882adc3888f874c6a7a35d1f170c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Mailbox::Application.config.session_store :cookie_store, key: '_mailbox_session'
